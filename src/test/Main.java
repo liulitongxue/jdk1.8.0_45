0af49b1b0830cea9649e36f325eeab4a0750c8a1
liuli001001 <liulitongxue@126.com>
2021-07-17 09:07:17 +0800

    :beer: 初始化项目

---
+++ src/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+
+/**
+ * @Author liuli
+ * @Email liulitongxue@126.com
+ * @Date 2021年7月15日 下午10:53
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
